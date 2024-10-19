df5873475650d3dbfc89054aed617a796892945e
ayushjodd <ayushkalonia2016@gmail.com>
2024-10-19 14:53:57 +0530

    added root layout file

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,25 +1,16 @@
-import { Link, Slot } from "expo-router";
-import { View, Text } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { useWallpapers } from "@/hooks/useWallpapers";
+import { Slot, Stack } from "expo-router";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
-export const _layout = () => {
+export default function Layout() {
+  const walletpapers = useWallpapers();
   return (
-    <SafeAreaView>
-      <View style={{ height: "90%" }}>
-        <Slot />
-      </View>
-      <View>
-        <Link href="/account">
-          <Text>take me to accounts page</Text>
-        </Link>
-        <Link href="/">
-          <Text>take me to fore you page</Text>
-        </Link>
-        cd
-        <Link href="/explore">
-          <Text>take me to explore page</Text>
-        </Link>
-      </View>
-    </SafeAreaView>
+    <GestureHandlerRootView>
+      <Stack
+        screenOptions={{
+          headerShown: false,
+        }}
+      ></Stack>
+    </GestureHandlerRootView>
   );
-};
+}
